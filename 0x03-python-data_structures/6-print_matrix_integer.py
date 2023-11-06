f4608204d59a3b1dad7a8cf807f9657c46cac129
Larst7 <lasgwenzi7@gmail.com>
2023-11-06 12:28:42 -0800

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def no_c(my_string):
-    chars = len(my_string)
-    buf = ""
-    for i in range(0, chars):
-        if my_string[i] == "c" or my_string[i] == "C":
-            continue
-        buf = buf + my_string[i]
-    return buf
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,17 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct listint_s - This is singly linked list
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
