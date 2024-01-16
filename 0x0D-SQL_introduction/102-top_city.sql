1962ae4b017f095e55e5c017def9e0eca0f0f11d
Larst7 <lasgwenzi7@gmail.com>
2024-01-16 14:03:20 -0800

    SQL_introduction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,13 +1,10 @@
 -- Assuming you have already created the necessary table structure
 USE hbtn_0c_0;
 
--- Import the table dump (replace 'dump_filename.sql' with the actual filename)
-SOURCE /path/to/dump_filename.sql;
-
 -- Query to display the top 3 cities' temperatures during July and August
-SELECT city, AVG(temperature) as avg_temp
+SELECT city, temperature
 FROM temperatures
 WHERE month IN ('July', 'August')
-GROUP BY city
-ORDER BY avg_temp DESC
+ORDER BY temperature DESC
 LIMIT 3;
+
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,9 +1,6 @@
 -- Assuming you have already created the necessary table structure
 USE hbtn_0c_0;
 
--- Import the table dump (replace 'dump_filename.sql' with the actual filename)
-SOURCE /path/to/dump_filename.sql;
-
 -- Query to display the max temperature of each state ordered by state name
 SELECT state, MAX(temperature) AS max_temp
 FROM temperatures
