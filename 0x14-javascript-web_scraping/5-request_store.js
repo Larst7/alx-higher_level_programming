362dc2efd64193fc475847d6ea10eb15a43429e4
Larst7 <lasgwenzi7@gmail.com>
2024-03-26 21:47:30 +0200

    a script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,17 +1,23 @@
 #!/usr/bin/node
-// lorem ipsum
 
 const request = require('request');
 const fs = require('fs');
 
-request.get(process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
-      if (error) {
-        console.log(error);
-      }
-    });
+const args = process.argv;
+const url = args[2];
+const filePath = args[3];
+
+request(
+  {
+    method: 'GET',
+    uri: url
+  },
+  function (error, response, body) {
+    if (error) throw error;
+    if (response.statusCode === 200) {
+      fs.writeFile(filePath, body, 'utf8', function (error) {
+        if (error) throw error;
+      });
+    }
   }
-});
+);
