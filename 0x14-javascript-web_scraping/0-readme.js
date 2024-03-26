5ca662b87a48e1e14bd9b8c87dccc6a8b80dd1d7
Larst7 <lasgwenzi7@gmail.com>
2024-03-26 21:09:50 +0200

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs');
+// Read from file
 
-fs.readfile(process.argv[2], 'utf8', (err, data) => {
-	if (err) {
-		return console.log(err);
-	}
-	process.stdout.write(data);
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });;
